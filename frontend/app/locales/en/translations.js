4ac5803dfddd9e5a68c09d3409a4021b4371543f
GCorbel <guirec.corbel@gmail.com>
2016-09-28 14:52:53 -0400

    Use I18n

---
+++ frontend/app/locales/en/translations.js
@@ -0,0 +1,19 @@
+export default {
+  members: {
+    index: {
+      edit: 'Edit',
+      delete: 'Delete',
+      add: 'Add a member'
+    },
+    edit: {
+      name: 'Name',
+      email: 'Email',
+      submit: 'Submit'
+    },
+    new: {
+      name: 'Name',
+      email: 'Email',
+      submit: 'Submit'
+    }
+  }
+};
